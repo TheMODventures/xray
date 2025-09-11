8d2e6e9c71c28bfddb2b8b52ae8d693c96fcda7d
AbdulRehman04-11-2002 <rehmanismail920@gmail.com>
2025-09-12 02:25:32 +0500

    Add API rewrite rule for disease detection endpoint in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/detect-disease',
+        destination: 'http://128.199.30.51:9000/detect-disease',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
