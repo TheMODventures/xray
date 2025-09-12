154e8c4062cc49f5803934513b1a2e132aedb486
TheMODventures <themodventures2023@gmail.com>
2025-09-12 20:11:55 +0500

    Update page.tsx

--- app/(Public)/login/page.tsx
+++ app/(Public)/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from '@/components/forms/auth/login/login.form';
 
 export default function LoginPage() {
   return (
-    <div className="min-h-screen bg-white flex items-center justify-center">
+     <div className="min-h-screen bg-white flex items-center justify-center">
       <div className="max-w-md w-full space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
